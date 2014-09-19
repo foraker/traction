f13bf979965ef64ac746604501d4ec4443a93bfd
Ben Eddy <bae@foraker.com>
2014-09-19 11:09:35 -0600

    Move Rails.Model specs to Traction.Model

--- coffee/spec/model.spec.coffee
+++ coffee/spec/model.spec.coffee
@@ -3,19 +3,185 @@ class TestClass extends Traction.Model
     fullName: -> [@get("firstName"), @get("lastName")].join(" ")
   }
 
-describe "computed attributes", ->
-  beforeEach ->
-    @model = new TestClass({firstName: "first", lastName: "last"})
+describe "Traction.Model", ->
+  describe "computed attributes", ->
+    beforeEach ->
+      @model = new TestClass({firstName: "first", lastName: "last"})
 
-  it "should accurately compute the property", ->
-    expect(@model.get("fullName")).toBe("first last")
+    it "should accurately compute the property", ->
+      expect(@model.get("fullName")).toBe("first last")
 
-  it "updates the when a dependency changes", ->
-    @model.set(firstName: "updated first name")
-    expect(@model.get("fullName")).toBe("updated first name last")
+    it "updates the when a dependency changes", ->
+      @model.set(firstName: "updated first name")
+      expect(@model.get("fullName")).toBe("updated first name last")
 
-  it "emits change events", ->
-    spy = jasmine.createSpy()
-    @model.on("change:fullName", spy)
-    @model.set(firstName: "updated first name")
-    expect(spy).toHaveBeenCalled()
+    it "emits change events", ->
+      spy = jasmine.createSpy()
+      @model.on("change:fullName", spy)
+      @model.set(firstName: "updated first name")
+      expect(spy).toHaveBeenCalled()
+
+  describe "#url", ->
+    it "returns the 'url' attribute", ->
+      model = new Traction.Rails.Model(url: "/items")
+      expect(model.url()).toBe "/items"
+
+  describe "#toggle", ->
+    it "inverts an attribute", ->
+      model = new Traction.Rails.Model(admin: true)
+      model.toggle('admin')
+      expect(model.get("admin")).toBe false
+
+  describe "#set", ->
+    class User extends Traction.Model
+      associations: {
+        comments: Backbone.Collection
+        profile: Backbone.Model
+      }
+
+    beforeEach ->
+      @user = new User({
+        name: "Test User"
+        comments: [{content: "comment 1"}]
+        comments_url: "/comments"
+      })
+
+    describe "creating associations", ->
+      it "assigns attributes", ->
+        expect(@user.get("name")).toBe "Test User"
+
+      it "maps the associated attribute to the correct class", ->
+        expect(@user.get("comments")).toBeInstanceOf Backbone.Collection
+
+      it "assigns the correct attributes to the association", ->
+        expect(@user.get("comments").first().get("content")).toBe "comment 1"
+
+      it "assign a URL when present", ->
+        expect(@user.get("comments").url).toBe "/comments"
+
+      it "handles multiple associations", ->
+        user = new User(profile: {})
+        expect(user.get("profile")).toBeInstanceOf Backbone.Model
+
+    describe "updating associations", ->
+      it "sets associated model attributes", ->
+        @user.set({name: "new name"})
+        expect(@user.get("name")).toBe "new name"
+
+      it "sets associated collection contents", ->
+        @user.set({comments: []})
+        expect(@user.get("comments").models).toEqual []
+
+      it "retains the updated association", ->
+        @user.set({comments: []})
+        expect(@user.get("comments")).toBeInstanceOf Backbone.Collection
+
+      it "does not remove associations when the attribute has not been specified", ->
+        originalCollection = @user.get("comments")
+        @user.set()
+        expect(@user.get("comments")).toBe originalCollection
+
+      it "removes associations when the attribute is undefined", ->
+        @user.set(comments: undefined)
+        expect(@user.get("comments")).toBe undefined
+
+      it "sets setable (model) associations", ->
+        @user.set({profile: {type: "admin"}})
+        expect(@user.get("profile").get("type")).toBe "admin"
+
+      it "emits a change event", ->
+        callback = jasmine.createSpy()
+        @user.on("change:comments", callback)
+        @user.set({comments: []})
+        expect(callback).toHaveBeenCalled()
+
+      it "emits a change event when the association is removed", ->
+        callback = jasmine.createSpy()
+        @user.on("change:comments", callback)
+        @user.set({comments: null})
+        expect(callback).toHaveBeenCalled()
+
+      it "does not emit a change event when silent: true is passed", ->
+        callback = jasmine.createSpy()
+        @user.on("change:comments", callback)
+        @user.set({comments: []}, {silent: true})
+        expect(callback).not.toHaveBeenCalled()
+
+      it "does not emit a change event when the association is unchanged", ->
+        @user.set({profile: {type: "admin"}})
+        callback = jasmine.createSpy()
+        @user.on("change:profile", callback)
+        @user.set({profile: {type: "admin"}})
+        expect(callback).not.toHaveBeenCalled()
+
+      it "does not emit a change event when the lack of an association is unchanged", ->
+        @user.set({profile: null})
+        callback = jasmine.createSpy()
+        @user.on("change:profile", callback)
+        @user.set({profile: null})
+        expect(callback).not.toHaveBeenCalled()
+
+      describe "updating a nested association", ->
+        class Fees extends Traction.Rails.Collection
+
+        class Leg extends Traction.Rails.Model
+          associations: {
+            fees: Fees
+          }
+        class Legs extends Traction.Rails.Collection
+          model: Leg
+
+        class Move extends Traction.Rails.Model
+          associations: {
+            legs: Legs
+          }
+
+        beforeEach ->
+          fee = {id: 1}
+          leg = {id: 1, fees: [fee]}
+          @move = new Move(legs: [leg])
+
+        it "retains nested references", ->
+          fees = @move.get("legs").first().get("fees")
+          fee  = {id: 1}
+          leg  = {id: 1, fees: [fee]}
+          @move.set(legs: [leg])
+
+          expect(@move.get("legs").first().get("fees")).toBe fees
+
+        it "does not fire change events when the nested models are the same", ->
+          callback = jasmine.createSpy()
+          leg = @move.get("legs").first()
+          leg.on("change:fees", callback)
+          fee  = {id: 1}
+          leg  = {id: 1, fees: [fee]}
+          @move.set(legs: [leg])
+
+          expect(callback).not.toHaveBeenCalled()
+
+        it "retains references when values change", ->
+          fees = @move.get("legs").first().get("fees")
+          fee  = {id: 2}
+          leg  = {id: 1, fees: [fee]}
+          @move.set(legs: [leg])
+
+          expect(@move.get("legs").first().get("fees")).toBe fees
+
+        it "fires change events when the nested models are the same", ->
+          callback = jasmine.createSpy()
+          leg = @move.get("legs").first()
+          leg.on("change:fees", callback)
+          fee  = {id: 2}
+          leg  = {id: 1, fees: [fee]}
+          @move.set(legs: [leg])
+
+          expect(callback).toHaveBeenCalled()
+
+    it "handles attr, val style setting", ->
+      @user.set("name", "Timothy")
+      expect(@user.get("name")).toBe "Timothy"
+
+    it "does not mutate the attributes", ->
+      orignalAttributes = {comments: []}
+      @user.set(orignalAttributes)
+      expect(orignalAttributes).toEqual {comments: []}
--- spec/model.spec.js
+++ spec/model.spec.js
@@ -21,30 +21,342 @@
 
   })(Traction.Model);
 
-  describe("computed attributes", function() {
-    beforeEach(function() {
-      return this.model = new TestClass({
-        firstName: "first",
-        lastName: "last"
+  describe("Traction.Model", function() {
+    describe("computed attributes", function() {
+      beforeEach(function() {
+        return this.model = new TestClass({
+          firstName: "first",
+          lastName: "last"
+        });
+      });
+      it("should accurately compute the property", function() {
+        return expect(this.model.get("fullName")).toBe("first last");
+      });
+      it("updates the when a dependency changes", function() {
+        this.model.set({
+          firstName: "updated first name"
+        });
+        return expect(this.model.get("fullName")).toBe("updated first name last");
+      });
+      return it("emits change events", function() {
+        var spy;
+        spy = jasmine.createSpy();
+        this.model.on("change:fullName", spy);
+        this.model.set({
+          firstName: "updated first name"
+        });
+        return expect(spy).toHaveBeenCalled();
       });
     });
-    it("should accurately compute the property", function() {
-      return expect(this.model.get("fullName")).toBe("first last");
-    });
-    it("updates the when a dependency changes", function() {
-      this.model.set({
-        firstName: "updated first name"
+    describe("#url", function() {
+      return it("returns the 'url' attribute", function() {
+        var model;
+        model = new Traction.Rails.Model({
+          url: "/items"
+        });
+        return expect(model.url()).toBe("/items");
       });
-      return expect(this.model.get("fullName")).toBe("updated first name last");
     });
-    return it("emits change events", function() {
-      var spy;
-      spy = jasmine.createSpy();
-      this.model.on("change:fullName", spy);
-      this.model.set({
-        firstName: "updated first name"
+    describe("#toggle", function() {
+      return it("inverts an attribute", function() {
+        var model;
+        model = new Traction.Rails.Model({
+          admin: true
+        });
+        model.toggle('admin');
+        return expect(model.get("admin")).toBe(false);
       });
-      return expect(spy).toHaveBeenCalled();
+    });
+    return describe("#set", function() {
+      var User;
+      User = (function(_super) {
+        __extends(User, _super);
+
+        function User() {
+          return User.__super__.constructor.apply(this, arguments);
+        }
+
+        User.prototype.associations = {
+          comments: Backbone.Collection,
+          profile: Backbone.Model
+        };
+
+        return User;
+
+      })(Traction.Model);
+      beforeEach(function() {
+        return this.user = new User({
+          name: "Test User",
+          comments: [
+            {
+              content: "comment 1"
+            }
+          ],
+          comments_url: "/comments"
+        });
+      });
+      describe("creating associations", function() {
+        it("assigns attributes", function() {
+          return expect(this.user.get("name")).toBe("Test User");
+        });
+        it("maps the associated attribute to the correct class", function() {
+          return expect(this.user.get("comments")).toBeInstanceOf(Backbone.Collection);
+        });
+        it("assigns the correct attributes to the association", function() {
+          return expect(this.user.get("comments").first().get("content")).toBe("comment 1");
+        });
+        it("assign a URL when present", function() {
+          return expect(this.user.get("comments").url).toBe("/comments");
+        });
+        return it("handles multiple associations", function() {
+          var user;
+          user = new User({
+            profile: {}
+          });
+          return expect(user.get("profile")).toBeInstanceOf(Backbone.Model);
+        });
+      });
+      describe("updating associations", function() {
+        it("sets associated model attributes", function() {
+          this.user.set({
+            name: "new name"
+          });
+          return expect(this.user.get("name")).toBe("new name");
+        });
+        it("sets associated collection contents", function() {
+          this.user.set({
+            comments: []
+          });
+          return expect(this.user.get("comments").models).toEqual([]);
+        });
+        it("retains the updated association", function() {
+          this.user.set({
+            comments: []
+          });
+          return expect(this.user.get("comments")).toBeInstanceOf(Backbone.Collection);
+        });
+        it("does not remove associations when the attribute has not been specified", function() {
+          var originalCollection;
+          originalCollection = this.user.get("comments");
+          this.user.set();
+          return expect(this.user.get("comments")).toBe(originalCollection);
+        });
+        it("removes associations when the attribute is undefined", function() {
+          this.user.set({
+            comments: void 0
+          });
+          return expect(this.user.get("comments")).toBe(void 0);
+        });
+        it("sets setable (model) associations", function() {
+          this.user.set({
+            profile: {
+              type: "admin"
+            }
+          });
+          return expect(this.user.get("profile").get("type")).toBe("admin");
+        });
+        it("emits a change event", function() {
+          var callback;
+          callback = jasmine.createSpy();
+          this.user.on("change:comments", callback);
+          this.user.set({
+            comments: []
+          });
+          return expect(callback).toHaveBeenCalled();
+        });
+        it("emits a change event when the association is removed", function() {
+          var callback;
+          callback = jasmine.createSpy();
+          this.user.on("change:comments", callback);
+          this.user.set({
+            comments: null
+          });
+          return expect(callback).toHaveBeenCalled();
+        });
+        it("does not emit a change event when silent: true is passed", function() {
+          var callback;
+          callback = jasmine.createSpy();
+          this.user.on("change:comments", callback);
+          this.user.set({
+            comments: []
+          }, {
+            silent: true
+          });
+          return expect(callback).not.toHaveBeenCalled();
+        });
+        it("does not emit a change event when the association is unchanged", function() {
+          var callback;
+          this.user.set({
+            profile: {
+              type: "admin"
+            }
+          });
+          callback = jasmine.createSpy();
+          this.user.on("change:profile", callback);
+          this.user.set({
+            profile: {
+              type: "admin"
+            }
+          });
+          return expect(callback).not.toHaveBeenCalled();
+        });
+        it("does not emit a change event when the lack of an association is unchanged", function() {
+          var callback;
+          this.user.set({
+            profile: null
+          });
+          callback = jasmine.createSpy();
+          this.user.on("change:profile", callback);
+          this.user.set({
+            profile: null
+          });
+          return expect(callback).not.toHaveBeenCalled();
+        });
+        return describe("updating a nested association", function() {
+          var Fees, Leg, Legs, Move;
+          Fees = (function(_super) {
+            __extends(Fees, _super);
+
+            function Fees() {
+              return Fees.__super__.constructor.apply(this, arguments);
+            }
+
+            return Fees;
+
+          })(Traction.Rails.Collection);
+          Leg = (function(_super) {
+            __extends(Leg, _super);
+
+            function Leg() {
+              return Leg.__super__.constructor.apply(this, arguments);
+            }
+
+            Leg.prototype.associations = {
+              fees: Fees
+            };
+
+            return Leg;
+
+          })(Traction.Rails.Model);
+          Legs = (function(_super) {
+            __extends(Legs, _super);
+
+            function Legs() {
+              return Legs.__super__.constructor.apply(this, arguments);
+            }
+
+            Legs.prototype.model = Leg;
+
+            return Legs;
+
+          })(Traction.Rails.Collection);
+          Move = (function(_super) {
+            __extends(Move, _super);
+
+            function Move() {
+              return Move.__super__.constructor.apply(this, arguments);
+            }
+
+            Move.prototype.associations = {
+              legs: Legs
+            };
+
+            return Move;
+
+          })(Traction.Rails.Model);
+          beforeEach(function() {
+            var fee, leg;
+            fee = {
+              id: 1
+            };
+            leg = {
+              id: 1,
+              fees: [fee]
+            };
+            return this.move = new Move({
+              legs: [leg]
+            });
+          });
+          it("retains nested references", function() {
+            var fee, fees, leg;
+            fees = this.move.get("legs").first().get("fees");
+            fee = {
+              id: 1
+            };
+            leg = {
+              id: 1,
+              fees: [fee]
+            };
+            this.move.set({
+              legs: [leg]
+            });
+            return expect(this.move.get("legs").first().get("fees")).toBe(fees);
+          });
+          it("does not fire change events when the nested models are the same", function() {
+            var callback, fee, leg;
+            callback = jasmine.createSpy();
+            leg = this.move.get("legs").first();
+            leg.on("change:fees", callback);
+            fee = {
+              id: 1
+            };
+            leg = {
+              id: 1,
+              fees: [fee]
+            };
+            this.move.set({
+              legs: [leg]
+            });
+            return expect(callback).not.toHaveBeenCalled();
+          });
+          it("retains references when values change", function() {
+            var fee, fees, leg;
+            fees = this.move.get("legs").first().get("fees");
+            fee = {
+              id: 2
+            };
+            leg = {
+              id: 1,
+              fees: [fee]
+            };
+            this.move.set({
+              legs: [leg]
+            });
+            return expect(this.move.get("legs").first().get("fees")).toBe(fees);
+          });
+          return it("fires change events when the nested models are the same", function() {
+            var callback, fee, leg;
+            callback = jasmine.createSpy();
+            leg = this.move.get("legs").first();
+            leg.on("change:fees", callback);
+            fee = {
+              id: 2
+            };
+            leg = {
+              id: 1,
+              fees: [fee]
+            };
+            this.move.set({
+              legs: [leg]
+            });
+            return expect(callback).toHaveBeenCalled();
+          });
+        });
+      });
+      it("handles attr, val style setting", function() {
+        this.user.set("name", "Timothy");
+        return expect(this.user.get("name")).toBe("Timothy");
+      });
+      return it("does not mutate the attributes", function() {
+        var orignalAttributes;
+        orignalAttributes = {
+          comments: []
+        };
+        this.user.set(orignalAttributes);
+        return expect(orignalAttributes).toEqual({
+          comments: []
+        });
+      });
     });
   });
 
